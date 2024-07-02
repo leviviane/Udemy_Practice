06efd7276cd48d8136d0eab5618351a5a2c96476
Viviane Le <le_vviane@yahoo.com>
2024-07-01 21:50:28 -0700

    js practice

---
+++ 14Intro_JS/string_length.js
@@ -0,0 +1,12 @@
+var tweet = prompt("What is your tweet?")
+
+//! length example
+// var tweetCount = tweet.length
+// var wordCount = 280;
+// var countLeft = wordCount - tweetCount;
+
+// alert("You have written " + tweetCount + ", you have " + countLeft + " left.")
+
+//! slice example
+var sliceTweet = tweet(0, 280);
+alert(sliceTweet);
