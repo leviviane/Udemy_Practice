4f0acb22568d16c801bb14555c1500d051afc14c
Viviane Le <le_viviane@yahoo.com>
2024-07-09 16:47:02 -0700

    testing green garden push

--- 14Intro_JS/life_in_weeks.js
+++ 14Intro_JS/life_in_weeks.js
@@ -1,10 +1,10 @@
 //create an output of days, weeks, and months left til you are 90 years old
 
 function lifeInWeeks(age) {
-    var years = 90 - age;
-    var days = years * 365;
-    var weeks = years * 52
-    var months = years * 12
+    var yearsLeft = 90 - age;
+    var days = yearsLeft * 365;
+    var weeks = yearsLeft * 52
+    var months = yearsLeft * 12
 
     console.log("You have " + days + " days," + weeks + " weeks, and " + months + " months left.")
 }
