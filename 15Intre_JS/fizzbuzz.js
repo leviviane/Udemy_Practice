f2da45a416fd21c56c5dc28f2bdd59dfc1c95bc5
Viviane Le <le_viviane@yahoo.com>
2024-07-11 17:18:33 -0700

    fibonacci practice

--- 15Intre_JS/fizzbuzz.js
+++ 15Intre_JS/fizzbuzz.js
@@ -1,22 +1,46 @@
 //create a fizzbuzz for numbers divided by 3,5, and both
 
+//while loop
 let output = [];
 let count = 1;
 
 function fizzBuzz() {
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.push("FizzBuzz")
+    while(count <= 100) { //this will print fizzBuzz 100 times
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("FizzBuzz")
+        }
+        else if (count % 3 === 0) {
+            output.push("Fizz")
+        }
+        else if (count % 5 === 0) {
+            output.push("Buzz")
+        } else {
+            output.push(count)
+        }
+        count++; //removing this will have the while loop run infinitely
+        console.log(output)
     }
-    else if (count % 3 === 0) {
-        output.push("Fizz")
+}
+
+fizzBuzz()
+
+
+//for loop
+function fizzBuzz() {
+    for(let count = 1; count < 101; count++) { //this will print fizzBuzz 100 times
+        if (count % 3 === 0 && count % 5 === 0) {
+            output.push("FizzBuzz")
+        }
+        else if (count % 3 === 0) {
+            output.push("Fizz")
+        }
+        else if (count % 5 === 0) {
+            output.push("Buzz")
+        } else {
+            output.push(count)
+        }
+        console.log(output)
     }
-    else if (count % 5 === 0) {
-        output.push("Buzz")
-    } else {
-        output.push(count)
-    }
-    count++;
-    console.log(output)
 }
 
 fizzBuzz()
