f2c4f09e0bad4154c0bbcfe661eb9a9b2e5650b0
Viviane Le <le_viviane@yahoo.com>
2024-07-24 14:48:28 -0700

    added drum audios

--- 18AdvJS_DOM/drum_kit/index.js
+++ 18AdvJS_DOM/drum_kit/index.js
@@ -1,13 +1,55 @@
 let numOfDrumsButtons = document.querySelectorAll(".drum").length;
 
+
+
 for(let i = 0; i < numOfDrumsButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked!")
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        let buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                let crash = new Audio("sounds/crash.mp3")
+                crash.play();
+                break;
+
+             case "a":
+                let kickBass = new Audio("sounds/kick-bass.mp3")
+                kickBass.play();
+                break;
+
+            case "s":
+                let snare = new Audio("sounds/snare.mp3")
+                snare.play();
+                break;
+
+            case "d":
+                let tom1 = new Audio("sounds/tom-1.mp3")
+                tom1.play();
+                break;
+
+            case "j":
+                let tom2 = new Audio("sounds/tom-2.mp3")
+                tom2.play();
+                break;
+
+            case "k":
+                let tom3 = new Audio("sounds/tom-3.mp3")
+                tom3.play();
+                break;
+
+            case "l":
+                let tom4 = new Audio("sounds/tom-4.mp3")
+                tom4.play();
+                break;
+
+
+            default: console.log(buttonInnerHTML)
+                break;
+        }
     })
 }
 
 
-
-
-
-
+// let audio = new Audio('sounds/tom-1.mp3')
+// audio.play()
